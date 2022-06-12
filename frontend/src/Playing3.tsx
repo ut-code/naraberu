980741971c0502bfef7859d9d088b4ab12ddb6af
宇田智哉 <udatomoya@udatomoyanoMacBook-Air.local>
2022-06-12 10:28:25 +0900

    level3の追加・アコーディオンメニューなど各種cssの編集を行いました。

--- frontend/src/Playing3.tsx
+++ frontend/src/Playing3.tsx
@@ -3,45 +3,48 @@ import './App.css' //cssを呼び出すときに必要
 
 
 let InitTerm = 0;
-const subtol = Math.floor( Math.random() * (5 - 1) ) + 1;
-const subfir = Math.floor( Math.random() * (10 - 1) ) + 1;
-const subfir2 = Math.floor( Math.random() * (3 - 1) ) + 1;
-function makeNewSequence(first, firstdif, tolerance, init){ //初めの文字列を生成
+const subtol = Math.floor( Math.random() * (3 - 1) ) + 1;
+const subfir = Math.floor( Math.random() * (5 - 1) ) + 1;
+const subfir2 = Math.floor( Math.random() * (5 - 1) ) + 1;
+function makeNewSequence(first, second, tolerance, init){ //初めの文字列を生成
   InitTerm = 0;
-  let putout = String(first);
-  let savenum = first;
-  let savedif = firstdif;
+  let putout = String(first+'. '+second);
+  let savenum = second;
+  let savefir = first;
   for (let i=1; i<init; i++){
-    savenum = savenum + savedif;
+    let storenum = savenum;
+    savenum = savenum + savefir + tolerance;
+    savefir = storenum;
     putout += ". "+String(savenum);
-    savedif = savedif + tolerance;
   }
   InitTerm = savenum;
-  return [putout,savedif];
+  return [putout,savefir];
 }
 
-export default function Playing2() {
-  const InitialNum = 5; //最初何項表示させるか
+export default function Playing3() {
+  const InitialNum = 6; //最初何項表示させるか
   //初項と公差は1〜9からランダム
-  const Tolerance = subtol; //階差の公差
-  const FirstTerm = subfir; //初項1
-  const FirstDifTerm = subfir2; //初項2(階差数列)
+  const Tolerance = subtol; //毎回足す項
+  const FirstTerm = subfir; //1項目
+  const SecondTerm = subfir2; //2項目
 
   const [NewAns, setNewAns] = useState(""); //ユーザーの回答
   const [score,setScore] = useState(0); //ユーザーのスコア
-  const [TheSequence, setTheSequence] = useState(makeNewSequence(FirstTerm,FirstDifTerm,Tolerance,InitialNum)[0]);  //数列を文字列で格納
+  const [TheSequence, setTheSequence] = useState(makeNewSequence(FirstTerm,SecondTerm,Tolerance,InitialNum)[0]);  //数列を文字列で格納
   const [TermNum, setTermNum] = useState(InitialNum);  //項の番号
   const [NowTerm, setNowTerm] = useState(InitTerm);  //最新の項の値(初期値は初項)
-  const [DifTerm, setDifTerm] = useState(makeNewSequence(FirstTerm,FirstDifTerm,Tolerance,InitialNum)[1]);  //階差数列の値(初期値は初項)
+  const [DifTerm, setDifTerm] = useState(makeNewSequence(FirstTerm,SecondTerm,Tolerance,InitialNum)[1]);  //最新の1つ前の値
   const [WrongState, setWrongState] = useState(false);  //間違えた状態(エラーメッセージを表示するかどうか)
 
   const FuncSubmit = () =>{ //提出を受けて対応する反応を返す
     const copiedSequence = TheSequence;
+    let storeNew = 0;
     CheckAns() ?
       (
+        storeNew = NowTerm,
         setNowTerm(CalTerms()),
         setTermNum(TermNum+1),
-        setDifTerm(DifTerm+Tolerance),
+        setDifTerm(storeNew),
         setTheSequence(copiedSequence+". "+NewAns),
         setScore(score+giveReward()),
         setWrongState(false)
@@ -57,7 +60,7 @@ export default function Playing2() {
   };
 
   const CalTerms = () =>{ //次の項を計算する
-    return NowTerm + DifTerm;
+    return NowTerm + DifTerm + Tolerance;
   };
   
   let get_text = document.getElementById("input");  //テキストボックスのidを取得
@@ -69,7 +72,7 @@ export default function Playing2() {
   }
 
   const giveReward = () =>{ //SCOREを決定
-    return TermNum*2;
+    return Math.floor(TermNum*5);
   }
 
   const doneAction=() =>{ //終了後
@@ -81,7 +84,7 @@ export default function Playing2() {
     <>
       <div className="titleArea">
         <div id="putLevel">Level 3</div>
-        <div id="LevelTitle">階差数列</div>
+        <div id="LevelTitle">三項間漸化式</div>
       </div>
       <div className="scoreArea">
         <div id="scoreTag">

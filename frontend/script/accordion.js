cc95209401c18f84e06e434e64d938fddbbf7393
Futaba-rgb <104970783+Futaba-rgb@users.noreply.github.com>
2022-06-12 15:02:53 +0900

    解説を追加・アコーディオンパネルの修正をしました。

--- frontend/script/accordion.js
+++ frontend/script/accordion.js
@@ -1,4 +1,6 @@
 //アコーディオンをクリックした時の動作
+var findElm2 = $('.expHead').next(".expMain");//直後のアコーディオンを行うエリアを取得し
+$(findElm2).slideUp();//アコーディオンの上下動作
 $('.expHead').on('click', function() {//タイトル要素をクリックしたら
     var findElm = $(this).next(".expMain");//直後のアコーディオンを行うエリアを取得し
     $(findElm).slideToggle();//アコーディオンの上下動作
